64888d9cb08dddf494984cd926d83034d21c57c9
Stewart Murrie <stewart@murrie.org>
2017-04-24 15:59:20 -0700

    Some more shuffling after noding.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.listen(3000, () => {
+    console.log("I'm listening!")
+})
+
