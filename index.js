4d8a2dd7465fa9c4d3094d41b9fef8cbf5592bef
Stewart Murrie <stewart@murrie.org>
2017-04-24 16:45:36 -0700

    SFD of getting the word of the day definition from the OED API.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+require('dotenv').config()
 const express = require('express')
+const fetch = require('node-fetch')
+
 const app = express()
 
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+
+
+app.get('/wotd', (req, res) => {
+    var request = {
+        headers: {
+            app_id: process.env.OED_APP_ID,
+            app_key: process.env.OED_APP_KEY
+        }
+    }
+    
+    fetch('https://od-api.oxforddictionaries.com/api/v1/entries/en/tumultuous', request)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            res.send(json)
+        })
+});
 
 app.listen(3000, () => {
     console.log("I'm listening!")
