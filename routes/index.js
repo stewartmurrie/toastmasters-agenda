f25b343025585e802b2242604534360e8b917827
Stewart Murrie <stewart@murrie.org>
2017-04-26 10:08:35 -0700

    Turned the agenda.html file into an hbs template, added a route to show it. No parsed data yet, just returns the static file.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/agenda', function(req, res, next) {
+  res.render('agenda', { title: 'Dolby Speakers Meeting' });
+});
+
 module.exports = router;
