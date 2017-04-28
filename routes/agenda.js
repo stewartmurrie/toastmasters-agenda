dc751045f29e6e51f88ae2fe89b34c9534dee374
Stewart Murrie <stew@murrie.org>
2017-04-28 12:02:51 -0700

    Fixed up a broken build (some modules weren't in package.json), and refactored out the agenda router to its own file.

---
+++ routes/agenda.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+const hbs = require('hbs');
+
+hbs.registerHelper('concat', (...args) => args.slice(0, -1).join(''));
+
+router.get('/', function (req, res, next) {
+
+  var request = {
+    headers: {
+      app_id: process.env.OED_APP_ID,
+      app_key: process.env.OED_APP_KEY
+    }
+  }
+
+  fetch('https://od-api.oxforddictionaries.com/api/v1/entries/en/ace', request)
+    .then(response => response.json())
+    .then(json => {
+
+      const results = json.results[0];
+      const lexicalEntries = results.lexicalEntries[0];
+      const entries = lexicalEntries.entries[0];
+      const senses = entries.senses[0];
+      const definitions = senses.definitions;
+      
+      res.render('agenda', {
+        title: 'Dolby Speakers Meeting',
+        'wotd-definition': results
+      });
+    });
+
+});
+
+module.exports = router;
