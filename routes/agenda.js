f61a8802b8c67a2ec1d6f6cbd75e3cc81543f729
Stewart Murrie <stew@murrie.org>
2017-04-28 13:19:57 -0700

    Demo code to grab meetings from airtable

--- routes/agenda.js
+++ routes/agenda.js
@@ -4,9 +4,36 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const hbs = require('hbs');
 
+const Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+const base = Airtable.base(process.env.AIRTABLE_APP_ID);
+
 hbs.registerHelper('concat', (...args) => args.slice(0, -1).join(''));
 
 router.get('/', function (req, res, next) {
+
+  base('Meetings').select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function(record) {
+          console.log('Retrieved', record.get('Date and Time'));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
 
 
   // Get the definition for the word of the day
