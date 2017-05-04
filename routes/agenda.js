362be2b5ee16535eec0ec08222273b8a80101757
Stewart Murrie <stewart@murrie.org>
2017-05-03 19:20:10 -0700

    WIP that's cleaning up the fetching code using asyncs

--- routes/agenda.js
+++ routes/agenda.js
@@ -14,20 +14,20 @@ const base = Airtable.base(process.env.AIRTABLE_APP_ID);
 hbs.registerHelper('concat', (...args) => args.slice(0, -1).join(''));
 hbs.registerHelper("inc", value => parseInt(value) + 1);
 
-async function getWotDDefinition(wotd) {
+const getWotDDefinition = async function (wotd) {
   const b = await fetch('https://od-api.oxforddictionaries.com/api/v1/entries/en/' + wotd, {
-      headers: {
-        app_id: process.env.OED_APP_ID,
-        app_key: process.env.OED_APP_KEY
-      }
-    });
+    headers: {
+      app_id: process.env.OED_APP_ID,
+      app_key: process.env.OED_APP_KEY
+    }
+  });
 
   const j = await b.json();
 
   return j.results[0];
 }
 
-router.get('/wotd', function (req, res, next) {  
+router.get('/wotd', function (req, res, next) {
   (async () => {
     try {
       const r = await getWotDDefinition('ace')
@@ -39,96 +39,103 @@ router.get('/wotd', function (req, res, next) {
 });
 
 
-router.get('/', function (req, res, next) {
-  const meetingDetails = {};
+router.get('/', async function (req, res, next) {
+  try {
+    const meetingDetails = {};
 
-  const p1 = base('Meetings').select({ view: 'Grid view' }).all().then(meetingRecords => {
+    // Await the result of getting the meeting details before moving on.
+    const meetingRecords = await base('Meetings').select({ view: 'Grid view' }).all();
+
+    // Fill in the basics for the meeting with what we have so far.
     const meeting = meetingRecords[0];
-    const word = meeting.get('Word of the Day');
-    const location = meeting.get('Location');
-    const date = new Date(meeting.get('Date and Time')).toLocaleDateString();
-    const topic = meeting.get('Topic');
+    meetingDetails.location = meeting.get('Location');
+    meetingDetails.date = new Date(meeting.get('Date and Time')).toLocaleDateString();
+    meetingDetails.topic = meeting.get('Topic');
+    meetingDetails.wotd = meeting.get('Word of the Day');
 
-    meetingDetails.wotd = word;
-    meetingDetails.location = location;
-    meetingDetails.date = date;
-    meetingDetails.topic = topic;
+    // This is as much as we can get directly from the meeting. The rest will need to come from other database calls.
+    // These can be done in parallel, with a Promise.all();
+    // const tmP = base('Members').find(meeting.get('Toastmaster'));
+    // const topicmP = base('Members').find(meeting.get('TopicsMaster'));
+    // const geP = base('Members').find(meeting.get('General Evaluator'));
+    // const timerP = base('Members').find(meeting.get('Timer'));
+    // const ahP = base('Members').find(meeting.get('Ah-counter'));
+          
+    let p = ['Toastmaster', 'TopicsMaster', 'General Evaluator', 'Timer', 'Ah-counter'].map(role => base('Members').find(meeting.get(role)));
+    p.push(getWotDDefinition(meetingDetails.wotd))
 
-    const wotdP = fetch('https://od-api.oxforddictionaries.com/api/v1/entries/en/' + word, {
-      headers: {
-        app_id: process.env.OED_APP_ID,
-        app_key: process.env.OED_APP_KEY
-      }
-    });
-
-    // Get the basic functionaries
-    const tmP = base('Members').find(meeting.get('Toastmaster'));
-    const topicmP = base('Members').find(meeting.get('TopicsMaster'));
-    const geP = base('Members').find(meeting.get('General Evaluator'));
-    const timerP = base('Members').find(meeting.get('Timer'));
-    const ahP = base('Members').find(meeting.get('Ah-counter'));
-
-    // Now to get the speeches. These are a little tricker because (a) it's an array, and (b) there's more nested data to be fetched within these.
-    const speechPs = meeting.get('Speeches').map(speechID => base('Speeches').find(speechID));
-
-    return Promise.all([wotdP, tmP, topicmP, geP, timerP, ahP, ...speechPs]);
-
-  }).then(([wotdBody, toastmaster, topicsmaster, genevalEval, timer, ahCounter, ...speeches]) => {
-
-    // These details are available now, so stash them in shared state.
+    const [toastmaster, topicsmaster, generalEvaluator, timer, ahCounter, wotdDefinition] = await Promise.all(p);
+    meetingDetails.wotdDefinition = wotdDefinition;
     meetingDetails.toastmaster = toastmaster.get('Name');
     meetingDetails.topicsmaster = topicsmaster.get('Name');
-    meetingDetails.generalEvaluator = genevalEval.get('Name');
+    meetingDetails.generalEvaluator = generalEvaluator.get('Name');
     meetingDetails.timer = timer.get('Name');
     meetingDetails.ahCounter = ahCounter.get('Name');
-    meetingDetails.speeches = [];
-
-    // Some more things require more fetching, so Promisify these now. E.g., the WOTD response which is just a body just now.
-    const promises = [wotdBody.json()];
-
-    speeches.forEach(speech => {
-      const speechDetails = {};
-
-      speechDetails.title = speech.get('Title');
-
-      promises.push(base('Members').find(speech.get('Speaker')));
-      promises.push(base('Members').find(speech.get('Evaluator')));
-      promises.push(base('Projects').find(speech.get('Project')));
-
-      meetingDetails.speeches.push(speechDetails);
-    });
-
-    return Promise.all(promises);
-
-  }).then(([wotdDefn, ...speechDeets]) => {
-    // Now we have everything we need to fill in the meeting details.
-    meetingDetails.wotdDefinition = wotdDefn.results[0];
-
-    // FIXME speechDeets has an implicit ordering of (speaker, evaluator). There's doubtless a better way.
-    // UGH recursion can't be the best way of doing this. This is like C :(
-    const projectPs = [];
-
-    const t = function (sePair, i) {
-      if (sePair.length == 0) return;
-
-      let [s, e, p, ...rest] = sePair;
-      meetingDetails.speeches[i].speaker = s.get('Name');
-      meetingDetails.speeches[i].evaluator = e.get('Name');
-      meetingDetails.speeches[i].project = p.get('Project ID');
-      meetingDetails.speeches[i].time = p.get('Time');
-
-      t(rest, i+1);
-    }
-
-    t(speechDeets, 0);
 
     res.render('agenda', {
       title: 'Dolby Speakers Meeting',
       meeting: meetingDetails
     });
-  }).catch(err => {
-    throw new Error(err);
-  });
+  } catch (err) {
+    console.error(err);
+  }
+
+
+  // // Now to get the speeches. These are a little tricker because (a) it's an array, and (b) there's more nested data to be fetched within these.
+  // const speechPs = meeting.get('Speeches').map(speechID => base('Speeches').find(speechID));
+
+  //   return Promise.all([wotdP, tmP, topicmP, geP, timerP, ahP, ...speechPs]);
+
+  // }).then(([wotdBody, toastmaster, topicsmaster, genevalEval, timer, ahCounter, ...speeches]) => {
+
+  //   meetingDetails.speeches = [];
+
+  //   // Some more things require more fetching, so Promisify these now. E.g., the WOTD response which is just a body just now.
+  //   // const promises = [wotdBody.json()];
+
+  //   speeches.forEach(speech => {
+  //     const speechDetails = {};
+
+  //     speechDetails.title = speech.get('Title');
+
+  //     promises.push(base('Members').find(speech.get('Speaker')));
+  //     promises.push(base('Members').find(speech.get('Evaluator')));
+  //     promises.push(base('Projects').find(speech.get('Project')));
+
+  //     meetingDetails.speeches.push(speechDetails);
+  //   });
+
+  //   return Promise.all(promises);
+
+  // }).then(([wotdDefn, ...speechDeets]) => {
+  //   // Now we have everything we need to fill in the meeting details.
+  //   // meetingDetails.wotdDefinition = wotdDefn.results[0];
+
+  //   // FIXME speechDeets has an implicit ordering of (speaker, evaluator). There's doubtless a better way.
+  //   // UGH recursion can't be the best way of doing this. This is like C :(
+  //   const projectPs = [];
+
+  //   const t = function (sePair, i) {
+  //     if (sePair.length == 0) return;
+
+  //     let [s, e, p, ...rest] = sePair;
+  //     meetingDetails.speeches[i].speaker = s.get('Name');
+  //     meetingDetails.speeches[i].evaluator = e.get('Name');
+  //     meetingDetails.speeches[i].project = p.get('Project ID');
+  //     meetingDetails.speeches[i].time = p.get('Time');
+
+  //     t(rest, i+1);
+  //   }
+
+  //   t(speechDeets, 0);
+
+  //   res.render('agenda', {
+  //     title: 'Dolby Speakers Meeting',
+  //     meeting: meetingDetails
+  //   });
+  // }).catch(err => {
+  //   throw new Error(err);
+  // });
 });
 
 module.exports = router;
